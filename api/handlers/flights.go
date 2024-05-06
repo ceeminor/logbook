972768866346396eff89e1f3af1e33ddd4a6b654
Chris Minor <75701956+ceeminor@users.noreply.github.com>
2024-05-05 22:06:54 -0400

    Update flights.go

--- api/handlers/flights.go
+++ api/handlers/flights.go
@@ -29,7 +29,7 @@ func AddFlight(c *fiber.Ctx) error {
         	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
 			"error": err.Error(),
 		})
-    }
+    	}
 
 	database.DB.Db.Create(&flight)
 
