8cdd3a40f9095e631ad0544245b84bb83d4d46ba
Chris Minor <75701956+ceeminor@users.noreply.github.com>
2024-05-05 22:04:57 -0400

    Update main.go

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
     "github.com/gofiber/fiber/v2"
-
+    
     "github.com/ceeminor/logbook/database"
 )
 
 func main() {
-
     database.ConnectDb()
-
+    
     app := fiber.New()
-
+    
     Routes(app)
-
+    
     app.Listen(":8000")
 }
